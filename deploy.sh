eba9d7bcc2a1f5fb6d974216deb184c202fd7227
zhujianxiong <498637863@qq.com>
2022-08-23 19:00:37 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f https://github.com/zhujianxiong.github.io.git main
+git push -f https://github.com/zhujianxiong/zhujianxiong.github.io.git main
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main
